b2b6d81b307d68f6c5bfcf643adcb86701480f0e
noahbachmann <noah.bachmann@kauz.ch>
2024-11-14 13:29:07 +0100

    feat: player shooting

--- Scripts/Player.py
+++ Scripts/Player.py
@@ -1,9 +1,11 @@
 import pygame
+from AssetsManager import LASER_IMAGE as shot
 
 class Player:
     def __init__(self, x: float, y:float, image, size:tuple = None):
         self.direction = "top"
         self.speed = 100
+        self.shots = []
         if size:
             self.image = pygame.transform.scale(image, size)
         else:
@@ -11,29 +13,65 @@ class Player:
         self.rect = self.image.get_frect(center=(x, y))
 
     def draw(self, surface):
-        rotatedImage = self.getRotatedImage()
+        rotatedImage = RotateImageOfObject(self)
         surface.blit(rotatedImage, self.rect)
 
-    def getRotatedImage(self):
-        if self.direction == "top":
-            return self.image 
-        elif self.direction == "right":
-            return pygame.transform.rotate(self.image, -90)
-        elif self.direction == "bottom":
-            return pygame.transform.rotate(self.image, 180)
-        elif self.direction == "left":
-            return pygame.transform.rotate(self.image, 90)
-    
-    def getDirection(self):
-        if self.direction == "top":
-            return pygame.math.Vector2(0, -1)
-        elif self.direction == "right":
-            return pygame.math.Vector2(1, 0)
-        elif self.direction == "bottom":
-            return pygame.math.Vector2(0, 1)
-        elif self.direction == "left":
-            return pygame.math.Vector2(-1, 0)
-
-    def update(self, surface, dt):   
-        self.rect.center += self.getDirection() * self.speed * dt
+    def update(self, surface, dt): 
+        self.rect.center += GetDirection(self.direction) * self.speed * dt
         self.draw(surface)
+
+        keys = pygame.mouse.get_pressed()
+        if(keys[0]):
+            self.shoot()  
+        for shot in self.shots:
+            shot.update(surface, dt)
+    
+    def shoot(self):
+        if self.direction == "top":
+            self.shots.append(Shot(self.rect.midtop,self.direction,200,shot,(4,8)))
+        elif self.direction == "right":
+            self.shots.append(Shot(self.rect.midright,self.direction,200,shot,(4,8)))
+        elif self.direction == "bottom":
+            self.shots.append(Shot(self.rect.midbottom,self.direction,200,shot,(4,8)))
+        elif self.direction == "left":
+            self.shots.append(Shot(self.rect.midleft,self.direction,200,shot,(4,8)))
+
+class Shot:
+    def __init__(self, pos:tuple, direction:str, speed, image, size:tuple = None):
+        self.vectorDir = GetDirection(direction)
+        self.direction = direction
+        self.speed = speed
+        if size:
+            self.image = pygame.transform.scale(image, size)
+        else:
+            self.image = image
+        self.image = RotateImageOfObject(self)
+        self.rect = self.image.get_frect(center=pos)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+    
+    def update(self, surface, dt):   
+        self.rect.center += self.vectorDir * self.speed * dt
+        self.draw(surface)
+
+        
+def RotateImageOfObject(object):
+        if object.direction == "top":
+            return object.image 
+        elif object.direction == "right":
+            return pygame.transform.rotate(object.image, -90)
+        elif object.direction == "bottom":
+            return pygame.transform.rotate(object.image, 180)
+        elif object.direction == "left":
+            return pygame.transform.rotate(object.image, 90)
+
+def GetDirection(direction:str):
+        if direction == "top":
+            return pygame.math.Vector2(0, -1)
+        elif direction == "right":
+            return pygame.math.Vector2(1, 0)
+        elif direction == "bottom":
+            return pygame.math.Vector2(0, 1)
+        elif direction == "left":
+            return pygame.math.Vector2(-1, 0)
