06964aee521e54968f0aaa79c563151af379890b
noahbachmann <noah.bachmann@kauz.ch>
2024-11-14 16:18:47 +0100

    feat: added heavy attack

--- Scripts/Player.py
+++ Scripts/Player.py
@@ -1,6 +1,6 @@
 import pygame
 from Timer import *
-from AssetsManager import LASER_IMAGE as shot
+from AssetsManager import *
 
 class Player:
     def __init__(self, x: float, y:float, image, size:tuple = None):
@@ -8,6 +8,8 @@ class Player:
         self.speed = 100
         self.atkSpeed = 1
         self.atkTimer = Timer(self.atkSpeed)
+        self.heavyCd = 10
+        self.heavyCdTimer = Timer(self.heavyCd)
         self.shots = []
         if size:
             self.image = pygame.transform.scale(image, size)
@@ -29,18 +31,31 @@ class Player:
             self.atkTimer.activate()
         if self.atkTimer.active:
             self.atkTimer.update()
+        if keys[2] and not self.heavyCdTimer.active:
+            self.heavy()
+            self.heavyCdTimer.activate()
         for shot in self.shots:
             shot.update(surface, dt)
     
     def shoot(self):
         if self.direction == "top":
-            self.shots.append(Shot(self.rect.midtop,self.direction,200,shot,(4,8)))
+            self.shots.append(Shot(self.rect.midtop,self.direction,200,LASER_IMAGE,(4,8)))
         elif self.direction == "right":
-            self.shots.append(Shot(self.rect.midright,self.direction,200,shot,(4,8)))
+            self.shots.append(Shot(self.rect.midright,self.direction,200,LASER_IMAGE,(4,8)))
         elif self.direction == "bottom":
-            self.shots.append(Shot(self.rect.midbottom,self.direction,200,shot,(4,8)))
+            self.shots.append(Shot(self.rect.midbottom,self.direction,200,LASER_IMAGE,(4,8)))
         elif self.direction == "left":
-            self.shots.append(Shot(self.rect.midleft,self.direction,200,shot,(4,8)))
+            self.shots.append(Shot(self.rect.midleft,self.direction,200,LASER_IMAGE,(4,8)))
+    
+    def heavy(self):
+        if self.direction == "top":
+            self.shots.append(Heavy(self.rect.midtop,self.direction,200,HEAVY_IMAGE,(8,8)))
+        elif self.direction == "right":
+            self.shots.append(Heavy(self.rect.midright,self.direction,200,HEAVY_IMAGE,(8,8)))
+        elif self.direction == "bottom":
+            self.shots.append(Heavy(self.rect.midbottom,self.direction,200,HEAVY_IMAGE,(8,8)))
+        elif self.direction == "left":
+            self.shots.append(Heavy(self.rect.midleft,self.direction,200,HEAVY_IMAGE,(8,8)))
 
 class Shot:
     def __init__(self, pos:tuple, direction:str, speed, image, size:tuple = None):
@@ -61,6 +76,24 @@ class Shot:
         self.rect.center += self.vectorDir * self.speed * dt
         self.draw(surface)
 
+class Heavy:
+    def __init__(self, pos:tuple, direction:str, speed, image, size:tuple = None):
+        self.vectorDir = GetDirection(direction)
+        self.direction = direction
+        self.speed = speed
+        if size:
+            self.image = pygame.transform.scale(image, size)
+        else:
+            self.image = image
+        self.image = RotateImageOfObject(self)
+        self.rect = self.image.get_frect(center=pos)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+    
+    def update(self, surface, dt):   
+        self.rect.center += self.vectorDir * self.speed * dt
+        self.draw(surface)
         
 def RotateImageOfObject(object):
         if object.direction == "top":
