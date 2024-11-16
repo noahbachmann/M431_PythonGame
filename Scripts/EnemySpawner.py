99a823d00541bd9fb4e523ed8b0d50fbb85d5a7d
noahbachmann <noah.bachmann@kauz.ch>
2024-11-16 15:55:56 +0100

    feat: added random choice between enemies

--- Scripts/EnemySpawner.py
+++ Scripts/EnemySpawner.py
@@ -2,13 +2,19 @@ import pygame
 from Enemy import *
 from Timer import Timer
 from AssetsManager import ENEMY_IMAGE
-from random import choice
+from random import *
 
 class Spawner:
     def __init__(self, difficulty:str, player, groups):
         self.player = player
         self.enemyGroups = groups
         self.spawnPoints = []
+        self.enemies = [
+            {"class": BasicShooter, "weight": 50, "args": (2, 1, 1, 150, self.player,
+            ENEMY_IMAGE,self.enemyGroups, 200, (64, 64))}, 
+            {"class": BasicMelee, "weight": 50, "args": (2, 1, 200, self.player,
+            ENEMY_IMAGE, self.enemyGroups, (64, 64))},
+            ]
         self.spawnPoints.extend([(x, 0) for x in range(0, 800, 100)])
         self.spawnPoints.extend([(x, 800) for x in range(0,800, 100)])
         self.spawnPoints.extend([(0, y) for y in range(0, 800, 100)])
@@ -22,9 +28,18 @@ class Spawner:
         self.spawnTimer.update()
         
     def SpawnEnemy(self):
-        #Enemy(choice(self.spawnPoints), 3, 1, 80, self.player, ENEMY_IMAGE, self.enemyGroups, (64, 64))
-        BasicShooter(choice(self.spawnPoints), 2, 1, 1, 150, self.player, ENEMY_IMAGE, self.enemyGroups, 300, (64,64))
-
+        rollNum = randint(1,100)
+        enemiesLength = len(self.enemies)
+        currentWeight = 0
+        for x in range(enemiesLength):
+            currentWeight += self.enemies[x]["weight"] 
+            if rollNum < currentWeight:
+                self.enemies[x]["class"](
+                    choice(self.spawnPoints),  
+                    *self.enemies[x]["args"],                 
+                )
+                break
+            
     def SetDifficulty(self):
         if self.difficulty == "normal":
-            self.spawnRate = 3
+            self.spawnRate = 2
