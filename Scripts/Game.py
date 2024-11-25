64ed342a16ede66da487a5d0d05a1611daf501d6
noqx <noel.merz09@outlook.com>
2024-11-25 12:55:38 +0100

    feat: added MainMenu

--- Scripts/Game.py
+++ Scripts/Game.py
@@ -3,11 +3,15 @@ import sys
 import time
 from Settings import *
 from Round import *
+from GameMenus import *
 
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("My Space Shooter")
-gameState = {'gaming': True, 'quit': False}
+gameState = {'gaming': False, 'quit': False}
+
+
+
 while not gameState['quit']:
     if gameState['gaming']:
         round = Round()
@@ -22,6 +26,18 @@ while not gameState['quit']:
                     gameState['quit'] = True
                     continue       
             endGameMenu.draw()
-            pygame.display.update()
+            pygame.display.update(),
+
+    if not gameState['gaming']:
+        MainMenuGame = MainMenu(250, 250,(650,650), True, gameState, 5)
+        while MainMenuGame.enabled:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameState['quit'] = True
+                    continue 
+                screen.fill((7, 0, 25))
+                MainMenuGame.draw()
+                pygame.display.update()
+         
 
 pygame.quit()
--- Scripts/GameMenus.py
+++ Scripts/GameMenus.py
@@ -1,15 +1,18 @@
 import pygame
 from Button import *
-from Player import *
 
 class Menu:
-    def __init__(self, left, top, enabled, size:tuple = None):
+    def __init__(self, left, top, color = None, enabled = False, size:tuple = None):
         self.displaySurface = pygame.display.get_surface()
         self.left = left
         self.top = top
         self.buttons = []
         self.texts = []
         self.enabled = enabled
+        if color:
+            self.color = color
+        else:
+            self.color = (240,240,240)
         if size:
             self.size = size
             self.rect = pygame.FRect(self.left, self.top, size[0], size[1])
@@ -17,7 +20,7 @@ class Menu:
             self.rect = pygame.FRect(self.left, self.top, 500, 500)
 
     def draw(self):
-        pygame.draw.rect(self.displaySurface, (240,240,240), self.rect, 0, 0)
+        pygame.draw.rect(self.displaySurface, self.color, self.rect, 0, 0)
         for button in self.buttons:
             button.update(self.displaySurface)
         for text, textRect in self.texts:
@@ -25,7 +28,7 @@ class Menu:
 
 class EndGameMenu(Menu):
     def __init__(self, left, top, size:tuple, enabled, gameState, score):
-        super().__init__(left, top, enabled, size=size)
+        super().__init__(left, top, enabled=enabled, size=size)
         self.gameState = gameState
         self.buttons.append(Button((self.rect.centerx, self.rect.centery), text="Play Again", func=self.newGame))
         self.buttons.append(Button((self.rect.centerx, self.rect.centery + 96), text="Main Menu", func=self.mainMenu))
@@ -35,6 +38,33 @@ class EndGameMenu(Menu):
     
     def newGame(self):
         self.enabled = False
+
+    def mainMenu(self):
+        self.enabled = False
+        self.gameState['gaming'] = False
+    
+    def quitGame(self):
+        self.enabled = False
+        self.gameState['quit'] = True
+
+
+class MainMenu(Menu):
+    def __init__(self, left, top, size:tuple, enabled, gameState, score):
+        super().__init__(left, top, enabled=enabled, size=size)
+        self.gameState = gameState
+
+        
+        self.buttons.append(Button((self.rect.centerx, self.rect.centery), text="Play", func=self.newGame))   
+        self.buttons.append(Button((self.rect.centerx, self.rect.centery + 90), text="Settings", func=self.quitGame))   
+        self.buttons.append(Button((self.rect.centerx, self.rect.centery + 180), text="Exit", func=self.quitGame))   
+        self.texts.append((font.render(str("My Space Shooter"), False, (0,0,0)), None))
+        self.texts[0] = (self.texts[0][0], self.texts[0][0].get_frect(center=(self.rect.centerx, self.rect.centery - 90)))
+
+
+    def newGame(self):
+        self.enabled = False
+        self.gameState['gaming'] = True
+
 
     def mainMenu(self):
         self.enabled = False
