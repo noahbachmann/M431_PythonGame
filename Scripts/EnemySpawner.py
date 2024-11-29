ee9de325edef56e4e66973bfe75dbd6eaf87665c
Noah <trainpaths@gmail.com>
2024-11-29 07:05:19 +0100

    feat: added animations

--- Scripts/EnemySpawner.py
+++ Scripts/EnemySpawner.py
@@ -1,7 +1,7 @@
 import pygame
 from Enemy import *
 from Timer import Timer
-from AssetsManager import Apex, Arachnis, Brawler
+from AssetsManager import Apex, Arachnis, Brawler, Enemy_Explosion
 from random import *
 
 class Spawner:
@@ -12,10 +12,12 @@ class Spawner:
         self.enemies = [
             #self, pos:tuple, health, damage, gold, speed, atkSpeed, player, image, groups, range=None, size:tuple = None
             {"class": BasicShooter, "weight": 50, "args": (2, 1, 1, 150, 5, self.player,
-            Arachnis.ARACHNIS_1,self.enemyGroups, 400, (64, 64))}, 
+            Arachnis.ARACHNIS_1,{"idle":{"frames":[Arachnis.ARACHNIS_1,Arachnis.ARACHNIS_2], "speed":8},
+                                "death":{"frames":Enemy_Explosion.animationArray, "speed":5}},self.enemyGroups, 400, (64, 64))}, 
             #self, pos:tuple, health, damage, gold, speed, player, image, groups, size:tuple = None
             {"class": BasicMelee, "weight": 50, "args": (2, 1, 3, 200, self.player,
-            Brawler.BRAWLER_IDLE_1, self.enemyGroups, (64, 64))},
+            Brawler.BRAWLER_IDLE_1,{"idle":{"frames":[Brawler.BRAWLER_IDLE_1, Brawler.BRAWLER_IDLE_2, Brawler.BRAWLER_IDLE_3], "speed":8},
+                                "death":{"frames":Enemy_Explosion.animationArray, "speed":5}}, self.enemyGroups, (64, 64))},
             ]
         self.spawnPoints.extend([(x, -200) for x in range(-200, 1200, 100)])
         self.spawnPoints.extend([(x, 1200) for x in range(-200, 1200, 100)])
