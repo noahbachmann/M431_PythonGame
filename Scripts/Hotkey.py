bb16e50d40ec6708f1746057b7146e3079adb686
noqx <noel.merz09@outlook.com>
2024-12-16 09:19:31 +0100

    feat: keybind settings: esacpe the cancel changing

--- Scripts/Hotkey.py
+++ Scripts/Hotkey.py
@@ -10,59 +10,65 @@ class Hotkeys:
 
 
     def change_Hotkey_Up():
-        print("Press a key to set the binding...")
         while True:  
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return  
                     Scripts.DataManager.dataJson['Hotkey_Up'] = event.key
                     Scripts.DataManager.saveData()
                     return  
 
     def change_Hotkey_Down():
-        print("Press a key to set the binding...")
         while True:  
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return
                     Scripts.DataManager.dataJson['Hotkey_Down'] = event.key
                     Scripts.DataManager.saveData()
                     return  
-                
+
     def change_Hotkey_Left():
-        print("Press a key to set the binding...")
         while True:  
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return
                     Scripts.DataManager.dataJson['Hotkey_Left'] = event.key
                     Scripts.DataManager.saveData()
                     return  
-                
+
     def change_Hotkey_Right():
-        print("Press a key to set the binding...")
         while True:  
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return
                     Scripts.DataManager.dataJson['Hotkey_Right'] = event.key
                     Scripts.DataManager.saveData()
                     return  
-                
+
     def change_Hotkey_Boost():
-        print("Press a key to set the binding...")
         while True:  
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return
                     Scripts.DataManager.dataJson['Hotkey_Boost'] = event.key
                     Scripts.DataManager.saveData()
                     return  
-                
-                
+
     def change_Hotkey_close():
-        print("Press a key to set the binding...")
         while True:  
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return
                     Scripts.DataManager.dataJson['Hotkey_close'] = event.key
                     Scripts.DataManager.saveData()
                     return  
+
                 
 
 
